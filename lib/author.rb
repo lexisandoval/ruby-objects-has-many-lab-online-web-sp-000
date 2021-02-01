fcb6f569ec334732a45028c3ffe8ab42307d275b
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-01 17:24:33 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,17 @@
+class Author
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+  end
+
+  def posts
+    @posts
+  end
+end
